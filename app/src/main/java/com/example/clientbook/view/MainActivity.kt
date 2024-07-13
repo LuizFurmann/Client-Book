8f218303b477ce5783900e8ac98d51b7ec775840
Luiz Furmann <furmannluiz@gmail.com>
2024-07-12 21:55:40 -0300

    adicionado drawer
    Adicionado estrutura de drawer

--- app/src/main/java/com/example/clientbook/view/MainActivity.kt
+++ app/src/main/java/com/example/clientbook/view/MainActivity.kt
@@ -2,15 +2,51 @@ package com.example.clientbook.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.clientbook.R
 import com.example.clientbook.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var toggle: ActionBarDrawerToggle
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupDrawer()
     }
+
+    private fun setupDrawer(){
+        val draweLayout : DrawerLayout = binding.drawerLayout
+        val navView : NavigationView = binding.navView
+
+        toggle = ActionBarDrawerToggle(this,draweLayout, R.string.open, R.string.close)
+        draweLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+                R.id.nav_gym -> Toast.makeText(this, "teste", Toast.LENGTH_SHORT).show()
+                R.id.nav_auto_training -> Toast.makeText(this, "teste", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
+    }
+
 }
