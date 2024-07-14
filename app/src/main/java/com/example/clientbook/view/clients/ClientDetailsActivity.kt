f9a27593d951ede7963134a074fb764ba99339c8
Luiz Alberto Furmann <furmannluiz@gmail.com>
2024-07-14 16:02:48 -0300

    criado tela de detalhes do cliente

---
+++ app/src/main/java/com/example/clientbook/view/clients/ClientDetailsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.clientbook.view.clients
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.clientbook.R
+
+class ClientDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_client_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
